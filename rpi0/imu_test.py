a885ce2c39b7c5e0a8a8678133a81135d87593d7
skateorcry <90222845+skateorcry@users.noreply.github.com>
2024-11-10 12:44:43 -0500

    tilt limit negative values

--- rpi0/imu_test.py
+++ rpi0/imu_test.py
@@ -6,6 +6,6 @@ imu = IMU()
 while True:
     time.sleep(1)
     imu.read_data()
-    if imu.rotation[0] > 45 or imu.rotation[1] > 45 or imu.rotation[2] > 45:
+    if imu.rotation[0] > 45 or imu.rotation[0] < -45 or imu.rotation[1] > 45 or imu.rotation[1] < -45:
         print("we are in trouble")
     print(f'X: {imu.rotation[0]}\tY: {imu.rotation[1]}\tZ: {imu.rotation[2]}')
