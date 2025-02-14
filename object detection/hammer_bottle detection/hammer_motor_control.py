3a249ea756d18af8f0874c332c86a45b2167e2e0
Noah McCampbell <57782774+noahmccampbell@users.noreply.github.com>
2025-02-13 21:03:18 -0500

    added aruco motor control

--- object detection/hammer_bottle detection/hammer_motor_control.py
+++ object detection/hammer_bottle detection/hammer_motor_control.py
@@ -10,14 +10,26 @@ if __name__ == "__main__":
     predictor = predict_live.YOLOLivePredictor(model_path)
     
     while True:
-        # ret, frame = predictor.cap.read()
-        frame = predictor.getFrame()
-        cv2.imshow('YOLO Live Feed', frame)
+        ret, frame = predictor.cap.read()
+        if not ret:
+            break
         result = predictor.get_highest_confidence_bounding_box_area(frame)
         print('Area:', result)
+        x1, y1, x2, y2 = map(int, box)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color=(0, 255, 0), thickness=2)
+        cv2.imshow('YOLO Live Feed', frame)
+        cv2.waitKey(0)
+        
+        
 
-        if(result > 5000):
+        if(result < 10000):
             rovers.forward()
         else:
             rovers.stop()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+           break
+#
+    predictor.cap.release()
+    cv2.destroyAllWindows()
         
